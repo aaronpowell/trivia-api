d2ef50c51b47235a3d2cdb404b0523092068ed5c
Ismail ALABOU <alabou.ismail@gmail.com>
2020-12-26 22:20:24 +0100

    updated the response

--- GetAllQuestions/index.js
+++ GetAllQuestions/index.js
@@ -1,9 +1,16 @@
 const questions = require("../questions");
 
-module.exports = async function(context, req) {
+module.exports = async function (context, req) {
   context.log("Request for all questions");
 
   context.res = {
-    body: questions
+    body: questions.map(function (question) {
+    return {
+      question: question.question,
+      answers: question.incorrect_answers
+        .concat([question.correct_answers])
+        .sort()
+    };
+    })
   };
 };
