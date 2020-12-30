e51d900e2b2dd7d547a6ec2fec7ae72c8c3a00a8
Ismail ALABOU <alabou.ismail@gmail.com>
2020-12-30 20:11:34 +0100

    Revert "updated the response"
    This reverts commit d2ef50c51b47235a3d2cdb404b0523092068ed5c.

--- GetAllQuestions/index.js
+++ GetAllQuestions/index.js
@@ -1,16 +1,9 @@
 const questions = require("../questions");
 
-module.exports = async function (context, req) {
+module.exports = async function(context, req) {
   context.log("Request for all questions");
 
   context.res = {
-    body: questions.map(function (question) {
-    return {
-      question: question.question,
-      answers: question.incorrect_answers
-        .concat([question.correct_answers])
-        .sort()
-    };
-    })
+    body: questions
   };
 };
